4f4f532f8e391d6a454c6b24f309f442e1e43f54
test1 <testac175@outlook.com>
2025-06-28 11:06:35 +0530

    initial

---
+++ Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿using CalculatorLib;
+
+var calculator = new Calculator();
+
+Console.WriteLine("2 + 3 = " + calculator.Add(2, 3));
+Console.WriteLine("2 - 3 = " + calculator.Subtract(2, 3));
+Console.WriteLine("2 * 3 = " + calculator.Multiply(2, 3));
+Console.WriteLine("6 / 3 = " + calculator.Divide(6, 3));
---
+++ CalculatorLib.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorLib.Tests
+{
+    [TestClass]
+    public sealed class CalculatorTests
+    {
+        private readonly Calculator _calculator = new();
+
+        [TestMethod]
+        public void Add_ReturnsCorrectResult()
+        {
+            var result = _calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            var result = _calculator.Subtract(2, 3);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void Multiply_ReturnsCorrectResult()
+        {
+            var result = _calculator.Multiply(2, 3);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Divide_ReturnsCorrectResult()
+        {
+            var result = _calculator.Divide(6, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Divide_ByZero_ThrowsDivideByZeroException()
+        {
+            Assert.ThrowsException<DivideByZeroException>(() => _calculator.Divide(1, 0));
+        }
+    }
+}
